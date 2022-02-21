c934ceb1a024068094000505f3c0c196e43d056b
Hasan <netter23@gmail.com>
2022-02-22 01:31:33 +0700

    add socket io

---
+++ socket_io.js
@@ -0,0 +1,31 @@
+/**
+ * Socket.IO
+ */
+
+export default function (httpServer) {
+
+  global.IO = require("socket.io")(httpServer, {
+    cors: {
+      // origin: "http://localhost:36459",
+      origin: "*",
+    }
+  });
+
+  IO.on('connection', (socket) => {
+    const _clientId = socket.id;
+    console.log(`a user ${_clientId} connected`);
+    socket.on('disconnect', () => {
+      console.log(`a user ${_clientId} disconnected`);
+    });
+
+    socket.on('broadcast', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('broadcast', msg);
+    });
+
+    // TODO: add onAny for development mode
+    socket.onAny((event, ...args) => {
+      console.log(event, args);
+    });
+  });
+}
